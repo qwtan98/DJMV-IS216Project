d4c8ba6c38faf133e4fc44e645913cdaf3ec2c3b
Janeleq <janelim2001@gmail.com>
2022-10-23 01:56:09 +0800

    Added Icon

---
+++ mikro-app/src/components/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-footer class="fixed-bottom">
+      Mikro
+      <v-spacer></v-spacer>
+      &copy; {{ new Date().getFullYear() }}
+    </v-footer>
+</template>
+  
+<script>
+export default {
+    name: 'TheFooter'
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- mikro-app/src/main.js
+++ mikro-app/src/main.js
@@ -2,8 +2,18 @@ import { createApp } from 'vue'
 import "bootstrap/dist/css/bootstrap.css"
 import App from './App.vue'
 import router from './router/router'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faBook } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faBook)
 
 createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
 .use(router)
 .mount('#app')
 

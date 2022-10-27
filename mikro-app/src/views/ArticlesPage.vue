0234613fb3b0ce0d39e11dca4571c6fd1fb663ba
Janeleq <janelim2001@gmail.com>
2022-10-28 02:06:40 +0800

    Update ArticlesPage.vue

--- mikro-app/src/views/ArticlesPage.vue
+++ mikro-app/src/views/ArticlesPage.vue
@@ -1,18 +1,31 @@
 <template>
-    <div class="articles" style="margin-left: 78px;">
+    <div class="container" style="margin-left: 78px;">
         <h1>This is the Articles Page.</h1>
       
-        <select name="stock_region" v-model="selected_stock_region" @change="created">
-                <option disabled value="">Please select one</option>
-                <option v-for="region of regions" :value="region.value" :key="region.id">
-                {{ region.key }}
+        <select name="stock_region" v-model="selected_topic" @change="created">
+                <option disabled value="">Please select one topic</option>
+                <option v-for="topic of topics" :value="topic.value" :key="topic.id">
+                {{ topic.key }}
             </option>
         </select>
+        Sort: 
+        <button class="rounded-pill"  @click="changeStatus($event)" value="EARLIEST">
+            Earliest
+        </button>
+        <button class="rounded-pill" @click="changeStatus($event)" value="LATEST">
+            Latest
+        </button>
         <div id="content">
             <div v-for="article of articles" :key="article.id">
-                <img :src="article.image_url" style="width: 100px; height: 50px;" >
-                {{ article.id || article.title }}
-                Date {{ article.published_at}}
+                <div class="card mb-4">
+                    <img :src="article.banner_image" style="width: 25%;" >
+                    <div class="card-body">
+                        <h5 class="card-title">{{ article.title }}</h5>
+                        <p class="card-text">{{ article.summary }}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <br/>
+                </div>
             </div>
         </div>
     </div>
@@ -30,16 +43,17 @@
         {
             return {
                 articles: [],
-                regions: [{key: 'Australia (au)', value: 'au'}, 
-                        {key: 'Canada (ca)', value: 'ca'},
-                        {key: 'China (cn)', value: 'cn'},
-                        {key: 'Denmark (dk)', value: 'dk'},
-                        {key: 'Finland (fi)', value: 'fi'},
-                        {key: 'France (fr)', value: 'fr'},
-                        {key:  'India (in)', value: 'in'},
-                        {key:  'United States (us)', value: 'us'},
+                topics: [{key: 'Technology', value: 'technology'}, 
+                        {key: 'Financial Markets', value: 'financial_markets'},
+                        {key: 'Energy & Transportation', value: 'energy_transportation'},
+                        {key: 'Finance', value: 'finance'},
+                        {key: 'Life Sciences', value: 'life_sciences'},
+                        {key: 'Manufacturing', value: 'manufacturing'},
+                        {key:  'Real Estate', value: 'real_estate'},
+                        {key:  'Retail & Wholesale', value: 'retail_wholesale'},
                         ],
-                selected_stock_region: ''
+                selected_topic: '',
+                selected_status: ''
             }
         },
 
@@ -50,20 +64,31 @@
 
         methods:
         {
+            // 117L1A3KOH0DR96N. ALPHA VANTAGE API KEY
             created: function() {
                 console.log("--- Loading News ---")
-                console.log(this.selected_stock_region)
-                // let api_endpoint = 'https://api.marketaux.com/v1/news/all?symbols=TSLA,AMZN,MSFT&api_token=HsxIoJ9mPcGwUbyCJGCfNgiyLyi92MEDXpaH10zN'
-                let api_endpoint = `https://api.marketaux.com/v1/news/all?countries=${this.selected_stock_region}&filter_entities=true&limit=100&api_token=HsxIoJ9mPcGwUbyCJGCfNgiyLyi92MEDXpaH10zN`
+                console.log(this.selected_topic)
+      
+                let api_endpoint = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&topics=${this.selected_topic}&limit=20&sort=${this.selected_status}&apikey=117L1A3KOH0DR96N`
+                // Marketaux api key
+                // let api_endpoint = `https://api.marketaux.com/v1/news/all?countries=${this.selected_stock_region}&filter_entities=true&limit=20&sort=${sort_status}api_token=HsxIoJ9mPcGwUbyCJGCfNgiyLyi92MEDXpaH10zN`
                 axios.get(api_endpoint)
                 .then(response => {
-                    console.log(response.data.data)
-                    this.articles = response.data.data      
+                    console.log(response.data.feed)
+        
+                    this.articles = response.data.feed   
                 })
                 
                 .catch(error => {
                     console.log(error)
                 })
+            },
+
+            // Sort By Earliest or Latest Article Date
+            changeStatus: function(e) {
+                console.log("--- Change Display Status ---", e.target.value)
+                this.selected_status = e.target.value
+                this.created()
             }
         },
 

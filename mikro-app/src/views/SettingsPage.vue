441d8c5638d3e03a99d086dac4155df0299ba400
thedexterong <111336479+thedexterong@users.noreply.github.com>
2022-11-08 14:19:56 +0800

    Create SettingsPage.vue
    I have create setting page

---
+++ mikro-app/src/views/SettingsPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="settings" style="margin-left: 78px;">
+        <h1>This is the Settings Page.</h1>
+        <h2></h2>
+    </div>
+  </template>
+  
+<script>
+    export default 
+    {
+        name: 'SettingsPage',
+        props: 
+        {
+
+        }
+    }
+</script>

8d0c5a3e99efb9b298b51ed39d687c7a7f6bc4c9
QUAN_WAH TAN <qwtan98@gmail.com>
2022-11-10 18:38:08 +0800

    Progress page and firebase implementation

--- mikro-app/src/views/ProgressPage.vue
+++ mikro-app/src/views/ProgressPage.vue
@@ -1,17 +1,49 @@
 <template>
-    <div id="progress" style="margin-left: 78px;">
-        <h1>This is the Progress Page.</h1>
-        <h2>You will see ur achievements/badges, lesson progress here</h2>
+    <div id="progress" style="margin-left: 55px;">
+        <FirebaseDatabase></FirebaseDatabase>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="progress-page-tab col-6 text-center align-self-center">
+                    <span class>Achievements</span>
+                </div>
+                <div class="progress-page-tab col-6 text-center align-self-center">
+                    <span class>Achievements</span>
+                </div>
+            </div>
+        </div>
+        <div class="container" style="background-color:wheat">
+            <div class="row" style ="padding: 10px">
+                <div class="col-4" style ="height:125px">
+                    <img src="../assets/ui/chatur.webp" style="height:125px; width:125px"/>
+                </div>
+                <div class="col-8 d-flex-column align-self-end">
+                    <h2>Achievements Title</h2>
+                    <span>Progress bar</span>
+                </div>
+            </div>
+        </div>
     </div>
   </template>
   
 <script>
-    export default 
-    {
-        name: 'AchievementsPage',
-        props: 
-        {
+import FirebaseDatabase from '../components/FirebaseDatabase.vue';
 
+// Initialize Firebase
+
+    export default {
+        name: "AchievementsPage",
+    
+        props: {},
+    
+        components: { 
+            FirebaseDatabase 
         }
     }
 </script>
+
+<style>
+.progress-page-tab {
+    background-color:aquamarine;
+    justify-content: center;
+}
+</style>

270f4d7b19d3e0bfabe0f592504a17c0e01c02c9
Jane lim <janelim2001@gmail.com>
2022-11-11 17:51:21 +0800

    Update Translation
    updated footer + translation

--- mikro-app/src/views/HomePage.vue
+++ mikro-app/src/views/HomePage.vue
@@ -38,17 +38,28 @@
 
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped>
+  body {
+    top: 0 !important;
+  }
+
+  .skiptranslate {
+    display: none !important;
+  }
+
   h3 {
     margin: 40px 0 0;
   }
+
   ul {
     list-style-type: none;
     padding: 0;
   }
+
   li {
     display: inline-block;
     margin: 0 10px;
   }
+  
   a {
     color: #42b983;
   }

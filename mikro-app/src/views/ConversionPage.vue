6f64daa07d60d247b0168da9a896709ff1512049
Jane lim <janelim2001@gmail.com>
2022-11-11 18:38:20 +0800

    Update ConversionPage.vue

--- mikro-app/src/views/ConversionPage.vue
+++ mikro-app/src/views/ConversionPage.vue
@@ -2,18 +2,44 @@
     <NavigationBar/>
     <div id="progress" style="margin-left: 63px;">
         <h1>This is the Conversion Page.</h1>
-       
+        {{ rates }}
     </div>
   </template>
   
 <script>
-    import NavigationBar from '../components/NavigationBar.vue';
+import axios from 'axios';
+import NavigationBar from '../components/NavigationBar.vue';
 
     export default 
     {
     name: "ConversionPage",
     components: { NavigationBar },
+    
+    data() {
+        return {
+            rates: []
+        }
+    },  
+
     props: {},
     
+    methods: 
+    {
+        created: function(){
+            let api_endpoint = "https://eservices.mas.gov.sg/api/action/datastore/search.json?resource_id=10eafb90-11a2-4fbd-b7a7-ac15a42d60b6&limit=10&filters[end_of_month]=2022-10,2022-11"
+            axios.get(api_endpoint).then(response => {
+                console.log(response.data)
+                this.rates = response.data
+            })
+
+            .catch(error => {
+                    console.log(error)
+            })
+        }
+    },
+
+    beforeMount() {
+        this.created()
+    }
 }
 </script>

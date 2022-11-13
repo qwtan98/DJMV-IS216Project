9fefc7ce7e7672c6fb686d18af7b62693fe9da5f
QUAN_WAH TAN <qwtan98@gmail.com>
2022-11-13 14:08:31 +0800

    Authentication with Database
    implementation

--- mikro-app/src/main.js
+++ mikro-app/src/main.js
@@ -23,3 +23,55 @@ createApp(App)
 
 
 import "bootstrap/dist/js/bootstrap.js";
+
+import { initializeApp }from "firebase/app";
+// import { getDatabase, ref } from "firebase/database";
+import { getDatabase, ref } from "firebase/database";
+// import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut ,setPersistence,browserSessionPersistence} from "firebase/auth"
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqxHtE4yZbgxMaCY0QHV7j8E412OkNjHs",
+  authDomain: "djmv-is216project.firebaseapp.com",
+  projectId: "djmv-is216project",
+  storageBucket: "djmv-is216project.appspot.com",
+  messagingSenderId: "872537705550",
+  appId: "1:872537705550:web:1111aa58b1e0eece0bc71f",
+  measurementId: "G-KFK66MB8EG",
+  databaseURL: "https://djmv-is216project-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+
+export { database, auth, app, ref }
+// document.getElementById("databasetesting").addEventListener('click', (e) => {
+//       console.log("test")
+    //   var email = document.getElementById('email').value;
+    //   var password = document.getElementById('password').value;
+    //   var LoginStatus = "Logged in"
+    
+    //   signInWithEmailAndPassword(auth, email, password)
+    //     .then((userCredential) => {
+    //       // Signed in 
+    //       const user = userCredential.user;
+    
+    //       var dt = Date();
+    //       update(ref(database, 'Users in the system/' + user.uid), {
+    //         LoginTime: dt,
+    //         LoginStatus: LoginStatus,
+    //         email: email
+    //       })
+    
+          
+    //       window.location.href = '../homepageWithLogin/homepageWithLogin.html';
+    //       // ...
+    //     })
+    //     .catch((error) => {
+    //       const errorCode = error.code;
+    //       const errorMessage = error.message;
+    
+    //       alert(errorMessage);
+    //     });
+// });

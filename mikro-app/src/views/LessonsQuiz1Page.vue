d52830f7c4b7ca6ead74077055d80dcb1ec9865c
DEXTER ONG <dexter.ong.2021@scis.smu.edu.sg>
2022-11-13 17:03:21 +0800

    Added logic to quizzes

--- mikro-app/src/views/LessonsQuiz1Page.vue
+++ mikro-app/src/views/LessonsQuiz1Page.vue
@@ -11,7 +11,7 @@
                             <div class="card-body">
                                 <p class="card-text">Q{{index+1}}: {{value.Qn}}</p>
                                 <span v-for="que in value.Answers" :key="que.id">
-                                    <input type="checkbox"/>{{que}}<br>
+                                    <input type="checkbox" v-model="selected_ans" :value="que"/>{{que}}<br>
                                 </span>
                             </div>
                         </div>
@@ -19,7 +19,31 @@
                 </div>
             </span>
             <div>
-                <router-link to="Lessons" class="btn btn-primary" value="" style="margin-bottom: 10px">Submit Quiz</router-link>
+                <div class="btn btn-primary" href="#supportModal" data-bs-toggle="modal" style="margin-bottom: 10px">Submit Quiz</div>
+
+                <div class="modal fade" id="supportModal" tabindex="-1" aria-labelledby="supportModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="supportModalLabel">Support</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+
+                                    <div class="modal-body" v-if='selected_ans.includes("Inflation") && selected_ans.includes("2 - 2.5%") && selected_ans.includes("$1000") && selected_ans.includes("A cup of coffee in 1970 is worth $0.25. Now it is worth $1.59 in 2022.") && selected_ans.includes("Savings/Investment account")'>
+                                        You've got it all correct! Good job!<br><small>You can now return to the Lessons page.</small>
+                                    </div>
+
+                                    <div class="modal-body" v-else>Oh no! You've got a question or two wrong. Go back and try again!</div>
+
+                                    <div class="modal-footer">
+                                        <router-link to="Lessons" class="btn btn-secondary" value="" style="margin-bottom: 10px" v-if='selected_ans.includes("Inflation") && selected_ans.includes("2 - 2.5%") && selected_ans.includes("$1000") && selected_ans.includes("A cup of coffee in 1970 is worth $0.25. Now it is worth $1.59 in 2022.") && selected_ans.includes("Savings/Investment account")'>Return to Lessons</router-link>
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" v-else>Close</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
             </div>
         </div>
     </div>
@@ -45,7 +69,8 @@ import NavigationBar from '../components/NavigationBar.vue';
                     Answers: ["A cup of coffee in 1970 is worth $0.25. Now it is worth $1.59 in 2022.", "A bowl of noodles is $3.50. When I ask for extra noodles, the bowl now costs $4.00."]},
                     { Qn: "Where should I keep my savings to make sure my money doesn't lose its value?", 
                     Answers: ["Savings/Investment account", "Under my mattress"]}
-                ]
+                ],
+                selected_ans: [],           
             }
         }
     }
--- mikro-app/src/views/LessonsQuiz5Page.vue
+++ mikro-app/src/views/LessonsQuiz5Page.vue
@@ -2,7 +2,7 @@
     <NavigationBar/>
     <div id="lessonsquiz" style="margin-left: 58px;">
         
-        <div id="quiz5" class="container">
+        <div id="quiz1" class="container">
             <h1>Quiz 5: When should I get started?</h1>
             <span v-for="(value, index) in questions" :key="value.id">
                 <div class="row" style="margin-bottom: 10px;">
@@ -11,7 +11,7 @@
                             <div class="card-body">
                                 <p class="card-text">Q{{index+1}}: {{value.Qn}}</p>
                                 <span v-for="que in value.Answers" :key="que.id">
-                                    <input type="checkbox"/>{{que}}<br>
+                                    <input type="checkbox" v-model="selected_ans" :value="que"/>{{que}}<br>
                                 </span>
                             </div>
                         </div>
@@ -19,7 +19,30 @@
                 </div>
             </span>
             <div>
-                <router-link to="Lessons" class="btn btn-primary" value="" style="margin-bottom: 10px">Submit Quiz</router-link>
+                <div class="btn btn-primary" href="#supportModal" data-bs-toggle="modal" style="margin-bottom: 10px">Submit Quiz</div>
+
+                <div class="modal fade" id="supportModal" tabindex="-1" aria-labelledby="supportModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="supportModalLabel">Support</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+
+                            <div class="modal-body" v-if='selected_ans.includes("As soon as possible.") && selected_ans.includes("Yes, for a rainy day.")  && selected_ans.includes("No, because the market is volatile and I should make sensible decisions.")'>
+                                You've got it all correct! Good job!<br><small>You can now return to the Lessons page.</small>
+                            </div>
+
+                            <div class="modal-body" v-else>Oh no! You've got a question or two wrong. Go back and try again!</div>
+
+                            <div class="modal-footer">
+                                <router-link to="Lessons" class="btn btn-secondary" value="" style="margin-bottom: 10px" v-if='selected_ans.includes("As soon as possible.") && selected_ans.includes("Yes, for a rainy day.")  && selected_ans.includes("No, because the market is volatile and I should make sensible decisions.")'>Return to Lessons</router-link>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" v-else>Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -41,7 +64,8 @@ import NavigationBar from '../components/NavigationBar.vue';
                     Answers: ["Yes, for a rainy day.", "No, because I have liquid assets."]},
                     { Qn: "Should I invest money that I might need in the next 3-5 years?", 
                     Answers: ["Yes, because my investments will definitely pay off.", "No, because the market is volatile and I should make sensible decisions."]},
-                ]
+                ],
+                selected_ans: []
             }
         }
     }

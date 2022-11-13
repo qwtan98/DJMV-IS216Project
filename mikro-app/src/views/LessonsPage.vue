82fa3687360c0ba26816c252ab13fdcac391cf1c
QUAN_WAH TAN <qwtan98@gmail.com>
2022-11-13 18:20:12 +0800

    Update LessonsPage.vue
    added yt videos

--- mikro-app/src/views/LessonsPage.vue
+++ mikro-app/src/views/LessonsPage.vue
@@ -10,10 +10,12 @@
                     <div class="card" style="width: 100%;">
                         <!-- <img src="..." class="card-img-top" alt="..."> -->
                         <div class="card-body">
-                        <h5 class="card-title">Lesson {{ index + 1}}: {{ lesson.Title }}</h5>
-                        <p class="card-text" style="font-style:italic">{{ lesson.Subtitle }}</p>
-                        <router-link :to="'LessonsQuiz' + (index+1)" class="btn btn-primary" :value="index + 1">Go to Quiz {{index + 1}}</router-link>
-                        <!-- <LessonsQuizPage></LessonsQuizPage> -->
+                            <iframe width="100%" height="100%" style="aspect-ratio: 16 / 9;" :src="`https://www.youtube.com/embed/gFQNPmLKj1k?start=${lesson.Start}&end=${lesson.End}`" title="How to Invest for Beginners" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                            <h5 class="card-title">Lesson {{ index + 1}}: {{ lesson.Title }}</h5>
+                            <p class="card-text" style="font-style:italic">{{ lesson.Subtitle }}</p>
+                        
+                            <router-link :to="'LessonsQuiz' + (index+1)" class="btn btn-primary" :value="index + 1">Go to Quiz {{index + 1}}</router-link>
+
                         </div>
                     </div>
                 </div>
@@ -35,11 +37,11 @@
     data() {
         return {
             lessons: [
-                { Title: "What happens to my money over time?", Subtitle: "How inflation works" }, 
-                { Title: "Stop money from losing value over time!", Subtitle: "How to curb inflation" },
-                { Title: "How do I make money?", Subtitle: "Why just a savings account is insufficient" },
-                { Title: "What is an investment?", Subtitle: "" },
-                { Title: "When should I get started?", Subtitle: "" },
+                { Title: "What happens to my money over time?", Subtitle: "How inflation works", Start: 56, End: 116}, 
+                { Title: "Stop money from losing value over time!", Subtitle: "How to curb inflation", Start: 119, End: 154 },
+                { Title: "How do I make money?", Subtitle: "Why just a savings account is insufficient", Start: 156, End: 232 },
+                { Title: "What is an investment?", Subtitle: "", Start: 233, End: 333 },
+                { Title: "When should I get started?", Subtitle: "", Start: 1255, End: 1418 },
             ],
         };
     },
